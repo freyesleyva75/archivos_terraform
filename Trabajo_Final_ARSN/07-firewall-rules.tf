79843a9840b8c08c94c1779267c91c170f5ccb09
freyesleyva75 <freyesleyva75@gmail.com>
2024-11-19 09:54:26 +0100

    archivo firewall modificado

--- Trabajo_Final_ARSN/07-firewall-rules.tf
+++ Trabajo_Final_ARSN/07-firewall-rules.tf
@@ -43,13 +43,16 @@ resource "openstack_fw_policy_v2" "policy_out" {
   depends_on  = [openstack_fw_rule_v2.allow_internal]
 }
 
+data "openstack_networking_port_v2" "net1_port" {
+ name = "net1_port"
+}
+
 # Firewall grupo de asociacion de politicas y puerto de conexión con Net1
 resource "openstack_fw_group_v2" "firewall_group" {
   name                       = "firewall-group"
   ingress_firewall_policy_id = openstack_fw_policy_v2.policy_in.id
   egress_firewall_policy_id  = openstack_fw_policy_v2.policy_out.id
-  ports                      = [openstack_networking_port_v2.net1_port.id]
+  ports                      = [data.openstack_networking_port_v2.net1_port.id]
   depends_on                 = [openstack_fw_policy_v2.policy_in,
-                                openstack_fw_policy_v2.policy_out, 
-                                openstack_networking_port_v2.net1_port]
+                                openstack_fw_policy_v2.policy_out]
 }
