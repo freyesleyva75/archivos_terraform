6053a25b75a813aa1f12d2cfbb7a30b503b3cdd8
freyesleyva75 <freyesleyva75@gmail.com>
2024-11-19 10:09:32 +0100

    Actualizado el script de arranque

--- Trabajo_Final_ARSN/scripts/aranque_openstack.sh
+++ Trabajo_Final_ARSN/scripts/aranque_openstack.sh
@@ -5,5 +5,5 @@ sudo vnx -f openstack_lab.xml -x start-all,load-img
 sudo vnx_config_nat ExtNet $(ip route | grep default | cut -d" " -f 5)
 sudo vnx -f openstack_lab-terraform.xml --create
 sudo vnx -f openstack_lab-terraform.xml -x install-terraform
-cd /home/fredy.reyes/Descargas/archivos_terraform/
-cp * /mnt/tmp/openstack_lab-antelope_4n_classic_ovs-v04/shared
+cd /home/fredy.reyes/Documentos/archivos_terraform/Trabajo_Final_ARSN/
+cp -r * /mnt/tmp/openstack_lab-antelope_4n_classic_ovs-v04/shared
